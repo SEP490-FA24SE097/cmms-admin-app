01f43cecedaaebe79b4299911b3deb2ee992fb3d
datnt <dat.nt271102@gmail.com>
2024-12-15 04:15:31 +0700

    done nav-header

---
+++ app/(client)/manage/materials/page.tsx
@@ -0,0 +1,9 @@
+import NavHeader from "@/components/manager/nav-header/page";
+export default function ManageMaterialPage () {
+    return (
+      <div className="h-[100vh]">
+        <NavHeader/>
+        <div>Content </div>
+      </div>
+    );
+}
